4b4b34558bda1348fa43aadc294857f00d04bff2
jinmang2 <jinmang2@gmail.com>
2021-12-24 10:27:08 +0000

    upload configs

--- retro_reader/models/__init__.py
+++ retro_reader/models/__init__.py
@@ -1,3 +1,9 @@
+from .modeling_albert import (
+    AlbertConfig,
+    AlbertForSequenceClassification,
+    AlbertForQuestionAnsweringAVPool,
+    AlbertForQuestionAnsweringAVPoolBCEv3
+)
 from .modeling_electra import (
     ElectraConfig,
     ElectraForSequenceClassification,
